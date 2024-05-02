9a8de0d4b27f99a423bfa9045f74a725f1ec24bf
Henriquecimento <102530346+vscimento@users.noreply.github.com>
2024-05-02 01:05:11 +0100

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,54 @@
+whois = [
+  "<br>",
+  "Hello, i am Cimento",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Cimento?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+    '<span class="index">CIMENTO | Not A Corporation. All rights reserved.</span>',
+    "<br>",
+    
+    "   .o88b. d888888b .88b  d88. d88888b d8b   db d888888b  .d88b.  ",
+    "  d8P  Y8   `88'   88'YbdP`88 88'     888o  88 `~~88~~' .8P  Y8. ",
+    "  8P         88    88  88  88 88ooooo 88V8o 88    88    88    88 ",
+    "  8b         88    88  88  88 88~~~~~ 88 V8o88    88    88    88 ",
+    "  Y8b  d8   .88.   88  88  88 88.     88  V888    88    `8b  d8' ",
+    "   `Y88P' Y888888P YP  YP  YP Y88888P VP   V8P    YP     `Y88P'  ",
+
+    "<br>",
+
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
